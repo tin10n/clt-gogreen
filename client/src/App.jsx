78e5ea4652fb2ae98d99903f795703b1fcde7ce2
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-29 13:15:21 -0400

    header and footer + tailwind install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import Home from './pages/Home'
+
+import "./styles/global.css"
 
 function App() {
 	return (
-		<>
-			<SignUp />
-		</>
+	<Router>
+		<Header />
+		
+		<main>
+
+			<SignUp />				
+			
+			<Routes>
+			<Route path="/" element={<Home />} />
+
+			</Routes>
+		</main>
+
+		<Footer />
+	</Router>
 	);
 }
 
