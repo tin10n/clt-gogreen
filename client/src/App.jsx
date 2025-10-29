600b0f17704e15d625eabadeba0eaaa3e357f5eb
Jane Le <janehtle@gmail.com>
2025-10-29 17:34:30 -0400

    removed old code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,38 +27,3 @@ function App() {
 
 export default App
 
-
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-
-// import Login from './pages/Login';
-// import SignUp from './pages/SignUp';
-// import Home from './pages/Home'
-
-// import "./styles/global.css"
-
-// function App() {
-// 	return (
-// 	<Router>
-// 		<Header />
-		
-// 		<main>
-
-// 			<SignUp />				
-			
-// 			<Routes>
-// 			<Route path="/" element={<Home />} />
-
-// 			</Routes>
-// 		</main>
-
-// 		<Footer />
-// 	</Router>
-// 	);
-// }
-
-// export default App;
