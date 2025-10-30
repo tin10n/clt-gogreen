72b49cff0218720f3dd92101895afef2117264b2
Mhowey19 <michaelhowey5@gmail.com>
2025-10-30 11:21:12 -0400

    Added route to task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Layout from "./components/Layout";
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Layout from './components/Layout';
 import Leaderboard from './pages/Leaderboard';
-import "./styles/global.css"
+import './styles/global.css';
 
 //because I made a Layout component, we don't need to add Header and Footer component -JL
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Task from './components/Task';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Layout>
 				<Routes>
+					<Route path="/task" element={<Task />} />
 					<Route path="/" element={<Home />} />
 					<Route path="/signup" element={<SignUp />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/leaderboard" element={<Leaderboard />} />
-
 				</Routes>
 			</Layout>
 		</BrowserRouter>
-	)
+	);
 }
 
-export default App
-
+export default App;
